9fde883e9a1ead6df44aa71387faa09efe919597
Naza <santiagoherrera13579@gmail.com>
2026-04-26 23:23:19 +0000

    Version 1.0

---
+++ ProyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/Oficinas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectointegrador;
+
+/**
+ *
+ * @author Nazareno
+ */
+public class Oficinas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

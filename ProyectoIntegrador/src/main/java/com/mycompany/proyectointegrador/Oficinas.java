7c42a7fa4f1c90cf8e0fb6a20d7a858ba2318332
Naza <santiagoherrera13579@gmail.com>
2026-04-26 23:28:18 +0000

    Version 1.0

--- ProyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/Oficinas.java
+++ ProyectoIntegrador/src/main/java/com/mycompany/proyectointegrador/Oficinas.java
@@ -1,16 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.proyectointegrador;
 
-/**
- *
- * @author Nazareno
- */
+// Importamos una libreria de java para el manejo de Arrays
+import java.util.ArrayList;
+
+// Importamos una libreria de java para el manejo de Listas
+import java.util.List;
+
 public class Oficinas {
 
+    // Metodo main, desde aqui empieza la ejecucion del programa
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create a list of employees
+
+        //
+        //
+        //
+        // Se declara una lista
+        // "List" es una interfaz de la libreria
+        // "<Employee>" esto indica que la lista solo puede contener objetos de tipo
+        // "Employee"
+        // "employees" es el nombre de la lista
+        // "= new ArrayList<>" a lo anterior le asigna la lista
+        // "List.of(" crea una lista temporal para usarla con objetos ya cargados
+        // La lista crea objetos con sus parametros.
+
+        List<Employee> employees = new ArrayList<>(
+                List.of(
+                        // firstName, lastName, age, documentNumber, phoneNumber, address, email, active, salary
+                        new Employee("John", "Smith", "jdoe", 1000.0),
+                        new Employee("Jane", "Williams", "jane", 2000.0),
+                        new Employee("Joe", "Johnson", "joe", 3000.0),
+                        new Employee("Jill", "Anderson", "jill", 4000.0),
+                        new Employee("Jim", "Brown", "jim", 5000.0),
+                        new Employee("Jenny", "Taylor", "jenny", 6000.0),
+                        new Employee("Jen", "Clark", "jen", 7000.0),
+                        new Employee("Jenny", "Thomas", "jenny", 8000.0),
+                        new Employee("Jen", "Wilson", "jen", 9000.0)));
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# TrabajoPracticoOficina
+# Trabajo Practico Integrador
+# Tema: Gestion de una Oficina.
 Trabajo practico grupal de "Programación Orientada a Objetos".
+
+## Integrantes:
+    - Santiago Nazareno Herrera Cevalle
+    - Emiliano Valverde
+    - Santino Balmaceda
+    - Santiago Mattar
+    - Juan
+
+<br> 
+
+### Link del repositorio original del codigo:
+**## Link aqui ##**
+
+***
+<br> 
+
+## Documentacion del Trabajo realizado:
+
+
+ 
